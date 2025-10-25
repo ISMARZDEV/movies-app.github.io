b223b77597b6191ce60bcd2114e92789bd6a7784
Ismael Martínez <1077546@est.intec.edu.do>
2025-10-25 12:22:19 -0400

    feat: mejorar la estructura de HomeScreen y agregar indicador de carga

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,21 @@
 import { nowPlayingAction } from "@/core/actions/movies/now-playing.action";
 import React from "react";
+import "./global.css"; // Importar estilos de NativeWind
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
 const queryClient = new QueryClient();
 
 const RootLayout = () => {
-
   nowPlayingAction();
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </QueryClientProvider>
   );
 };
--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,19 +1,26 @@
-import { useMovies } from '@/presentation/hooks/useMovies';
-import React from 'react';
-import { Text, View } from 'react-native';
+import { useMovies } from "@/presentation/hooks/useMovies";
+import React from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const { nowPlayingQuery } = useMovies();
+  const safeArea = useSafeAreaInsets();
 
-    const { nowPlayingQuery } = useMovies();
-
-
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className="flex-1  justify-center items-center">
+        <ActivityIndicator color="purple" size={40} />
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>{JSON.stringify(nowPlayingQuery.data, null, 2)}</Text>
+    <View className="mt-2" style={{ paddingTop: safeArea.top }}>
+      <Text className="text-3xl font-bold px-4 mb-2">HomeScreen</Text>
+      {/* <Text >{JSON.stringify(nowPlayingQuery.data, null, 2)}</Text> */}
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
