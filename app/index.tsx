1f3667b443019b2c397eff8238188363473840cb
Ismael Martínez <1077546@est.intec.edu.do>
2025-10-25 11:49:50 -0400

    feat: agregar estructura básica de la aplicación y configurar consultas de películas

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,8 @@
-import { nowPlayingAction } from "@/core/actions/movies/now-playing.action";
+import { Redirect } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
-const App = () => {
-
-  nowPlayingAction();
-  return (
-    <SafeAreaView>
-      <View className="mt-6 mx-2.5 ">
-        <Text className="text-2xl font-bold mb-4 font-work-black text-primary">
-          ¡Hola Mundo!
-        </Text>
-      </View>
-    </SafeAreaView>
-  );
+const MoviesApp = () => {
+  return <Redirect href={"/home"} />;
 };
 
-export default App;
+export default MoviesApp;
