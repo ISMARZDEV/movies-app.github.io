08a514dbc104a27b8049137e00f1666394c6e70e
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2025-10-25 13:37:53 -0400

    feat: agregar componente MainSlideshow y MoviePoster, actualizar HomeScreen (#7)

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,4 +1,4 @@
-import MainSlideshow from "@/presentation/components/MainSlideshow";
+import MainSlideshow from "@/presentation/components/movies/MainSlideshow";
 import { useMovies } from "@/presentation/hooks/useMovies";
 import React from "react";
 import { ActivityIndicator, Text, View } from "react-native";
@@ -18,7 +18,7 @@ const HomeScreen = () => {
 
   return (
     <View className="mt-2" style={{ paddingTop: safeArea.top }}>
-      <Text className="text-3xl font-bold px-4 mb-2">HomeScreen</Text>
+      <Text className="text-3xl font-bold px-4 mb-2">Movies App</Text>
       {/* <Text >{JSON.stringify(nowPlayingQuery.data, null, 2)}</Text> */}
 
       <MainSlideshow movies={ nowPlayingQuery.data ?? [] } />
--- presentation/components/movies/MainSlideshow.tsx
+++ presentation/components/movies/MainSlideshow.tsx
@@ -2,6 +2,7 @@ import { Movie } from "@/infrastructure/interfaces/movie.interface";
 import React, { useRef } from "react";
 import { Text, useWindowDimensions, View } from "react-native";
 import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
+import MoviePoster from "./MoviePoster";
 
 interface Props {
   movies: Movie[];
@@ -15,7 +16,9 @@ const MainSlideshow = ({ movies }: Props) => {
       <Carousel
         ref={ref}
         data={movies}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => (
+          <MoviePoster id={item.id} poster={item.poster} />
+        )}
         width={200}
         height={350}
         style={{
