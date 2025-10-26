3ca1a07d51281d8c2d4fcc257f8a2dd8e410d7f1
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2025-10-25 22:03:23 -0400

    feat: agregar interfaz MovieDBMovieResponse y lógica para obtener detalles de películas por ID (#11)

---
+++ core/actions/movie/get-movie-by-id.action.ts
@@ -0,0 +1,19 @@
+import { MovieDBMovieResponse } from "@/infrastructure/interfaces/moviedb-movie-response";
+import { movieApi } from "../api/movie-api";
+import { CompleteMovie } from "@/infrastructure/interfaces/movie.interface";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mapper";
+
+export const getMovieByIdAction = async (id: number | string): Promise<CompleteMovie> => {
+  try {
+    const { data } = await movieApi.get<MovieDBMovieResponse>(`/${id}`);
+
+    const movie = MovieMapper.fronTheMovieDBToCompleteMovie(data)
+    
+    console.log(data);
+
+    return movie;
+  } catch (error) {
+    console.log(error);
+    throw "Cannot load movie by id";
+  }
+};
