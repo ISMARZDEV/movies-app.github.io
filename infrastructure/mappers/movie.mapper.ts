6a4a7a39ef3b42ca39a2018a1d771dd894af716f
Ismael Martínez <1077546@est.intec.edu.do>
2025-10-25 22:01:39 -0400

    feat: agregar interfaz MovieDBMovieResponse y lógica para obtener detalles de películas por ID

--- infrastructure/mappers/movie.mapper.ts
+++ infrastructure/mappers/movie.mapper.ts
@@ -1,4 +1,5 @@
-import { Movie } from "../interfaces/movie.interface";
+import { CompleteMovie, Movie } from "../interfaces/movie.interface";
+import { MovieDBMovieResponse } from "../interfaces/moviedb-movie-response";
 import { Result } from "../interfaces/moviedb-response";
 
 /**
@@ -17,4 +18,21 @@ export class MovieMapper {
       backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
     };
   };
+
+  static fronTheMovieDBToCompleteMovie = ( movie: MovieDBMovieResponse): CompleteMovie => {
+    return {
+      id: movie.id,
+      title: movie.title,
+      description: movie.overview,
+      releaseDate: new Date(movie.release_date),
+      rating: movie.vote_average,
+      poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+      genres: movie.genres.map(g => g.name),
+      duration: movie.runtime,
+      budget: movie.budget,
+      originalTitle: movie.original_title,
+      productionCompany: movie.production_companies.map((c) => c.name),
+    }
+  }
 }
